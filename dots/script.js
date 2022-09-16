39ddd034d97985f7a9820897a643c9721cefff0c
JessieCasey <kenysglobal@gmail.com>
2022-09-16 17:56:35 +0200

    refactoring

--- dots/script.js
+++ dots/script.js
@@ -1,19 +1,19 @@
-const dotsRecursive = (str, tmp = [str[0]]) => {
+const functionRecursive = (str, temp = [str[0]]) => {
     if (str.length === 1) {
-      return tmp;
+      return temp;
     }
   
     const char = str[1];
   
     str = str.slice(1);
   
-    const nextWithDots = tmp.slice().map((x) => x + "." + char);
-    const nextWithoutDots = tmp.map((x) => x + char);
+    const nextWithDots = temp.slice().map((x) => x + "." + char);
+    const nextWithoutDots = temp.map((x) => x + char);
   
-    const withoutDots = dotsRecursive(str, nextWithoutDots);
-    const withDots = dotsRecursive(str, nextWithDots);
+    const withoutDots = functionRecursive(str, nextWithoutDots);
+    const withDots = functionRecursive(str, nextWithDots);
   
     return [...withoutDots, ...withDots];
   };
 
-  console.log(dotsRecursive("ilovenode"))
+  console.log(functionRecursive("ilovenode"))
